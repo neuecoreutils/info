dd932c5804eef0172967250b7b33fb07a0a9f636
WDXP <wolfengarde@rediffmail.com>
2022-03-15 18:51:46 +0530

    Added  to uname.

--- Sources/uname/uname.swift
+++ Sources/uname/uname.swift
@@ -7,28 +7,19 @@ import Foundation
 
 var argv = CommandLine.arguments
 var argc = CommandLine.argc
-
-let OSName = ProcessInfo.processInfo.operatingSystemVersionString
-
-print(":::Legacy:::")
-print(OSName)
-
 var sysinfo = utsname()
 uname(&sysinfo)
 
 if uname(&sysinfo) < 0 {
-    print("uname: Error: Cannot stat.")
+    print("uname: Error: Cannot stat system info.")
     exit(1)
 }
 
-print(":::Modern:::")
-
-let modelCode = withUnsafePointer(to: &sysinfo.machine) {
+let mach = withUnsafePointer(to: &sysinfo.machine) {
     $0.withMemoryRebound(to: CChar.self, capacity: 1) {
         ptr in String.init(validatingUTF8: ptr)
     }
 }
-
 let sysname = withUnsafePointer(to: &sysinfo.sysname) {
     $0.withMemoryRebound(to: CChar.self, capacity: 1) {
         ptr in String.init(validatingUTF8: ptr)
@@ -50,8 +41,27 @@ let hostname = withUnsafePointer(to: &sysinfo.nodename) {
     }
 }
 
-print(modelCode!)
-print(release!)
-print(sysname!)
-print(version!)
-print(hostname!)
+if argc > 1 && argv[1] == "-s" {
+    print(sysname!)
+}
+else if argc > 1 && argv[1] == "-n" {
+    print(hostname!)
+}
+else if argc > 1 && argv[1] == "-r" {
+    print(release!)
+}
+else if argc > 1 && argv[1] == "-v" {
+    print(ver!)
+}
+else if argc > 1 && argv[1] == "-m" {
+    print(mach!)
+}
+else if argc > 1 && argv[1] == "--version" {
+    print("uname (neueCoreutils) 0.2a")
+    print("Copyright (C) 2022 WDXP.\n")
+    print("Licensed under the MIT License.")
+    
+}
+else {
+    print(sysname!)
+}
