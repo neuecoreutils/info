98011d39a78c20efd25892131a5bffdb6e89c403
WDXP <wolfengarde@rediffmail.com>
2022-03-15 18:36:26 +0530

    uname rewritten in Swift!

--- Sources/uname/uname.swift
+++ Sources/uname/uname.swift
@@ -5,24 +5,53 @@
 
 import Foundation
 
-#if os(macOS) || os(iOS)
-import Darwin
-#elseif os(Linux)
-import Glibc
-#endif
-
 var argv = CommandLine.arguments
 var argc = CommandLine.argc
-var systemInfo = utsname()
-uname(&systemInfo)
-let username = NSUserName() 
-let homedir = NSHomeDirectory()
-let OSName = ProcessInfo.processInfo.operatingSystemVersionString
-let hname = ProcessInfo.processInfo.hostName
-let osver = ProcessInfo.processInfo.operatingSystemVersion
 
-print("\(username)@\(hname)")
-print("----------------------")
-print("OS: \(OSName)")
-print("\(homedir)")
-print("\(username)")
+let OSName = ProcessInfo.processInfo.operatingSystemVersionString
+
+print(":::Legacy:::")
+print(OSName)
+
+var sysinfo = utsname()
+uname(&sysinfo)
+
+if uname(&sysinfo) < 0 {
+    print("uname: Error: Cannot stat.")
+    exit(1)
+}
+
+print(":::Modern:::")
+
+let modelCode = withUnsafePointer(to: &sysinfo.machine) {
+    $0.withMemoryRebound(to: CChar.self, capacity: 1) {
+        ptr in String.init(validatingUTF8: ptr)
+    }
+}
+
+let sysname = withUnsafePointer(to: &sysinfo.sysname) {
+    $0.withMemoryRebound(to: CChar.self, capacity: 1) {
+        ptr in String.init(validatingUTF8: ptr)
+    }
+}
+let release = withUnsafePointer(to: &sysinfo.release) {
+    $0.withMemoryRebound(to: CChar.self, capacity: 1) {
+        ptr in String.init(validatingUTF8: ptr)
+    }
+}
+let version = withUnsafePointer(to: &sysinfo.version) {
+    $0.withMemoryRebound(to: CChar.self, capacity: 1) {
+        ptr in String.init(validatingUTF8: ptr)
+    }
+}
+let hostname = withUnsafePointer(to: &sysinfo.nodename) {
+    $0.withMemoryRebound(to: CChar.self, capacity: 1) {
+        ptr in String.init(validatingUTF8: ptr)
+    }
+}
+
+print(modelCode!)
+print(release!)
+print(sysname!)
+print(version!)
+print(hostname!)
