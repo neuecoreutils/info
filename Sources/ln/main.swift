793c61b28e4683378d3e288a2d0f6607f2824117
MP Prabhanand <wdxp@rediffmail.com>
2022-03-26 21:03:42 +0530

    fix ln to 'working state'

--- Sources/ln/main.swift
+++ Sources/ln/main.swift
@@ -22,10 +22,13 @@ if CommandLine.argc == 1 {
 var argv = CommandLine.arguments
 var argc = CommandLine.argc
 
-
-if argc > 1 && argv[1] == "-s" {
-        try FM.createSymbolicLink(atPath: fpath, withDestinationPath: olink)
+if argc == 4 && argv[1] == "-s" {
+        try FM.createSymbolicLink(atPath: argv[2], withDestinationPath: argv[3])
 }
-if argc > 1 && argv[1] == "-l" {
-        try FM.linkItem(atPath: fpath, toPath: olink)
+if argc == 4 && argv[1] == "-l" {
+        try FM.linkItem(atPath: argv[2], toPath: argv[3])
+}
+
+if argc == 1 {
+        print("ln: no operand")
 }
