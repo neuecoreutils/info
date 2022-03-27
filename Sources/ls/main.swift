293c394a84a6667971092c0144403e0d979615c5
MP Prabhanand <wdxp@rediffmail.com>
2022-03-27 18:49:17 +0530

    rewriting ls from scratch

--- Sources/ls/main.swift
+++ Sources/ls/main.swift
@@ -10,33 +10,7 @@
 
 import Foundation
 
-var FM = FileManager.default
-var progName = CommandLine.arguments[0]
+var argc = CommandLine.argc
+var argv = CommandLine.arguments
 
-if CommandLine.argc == 1 {
-        let path = FileManager.default.currentDirectoryPath
-        let fnf = try FM.contentsOfDirectory(atPath: path)        
-        print("Listing files in directory \(path).")
-        
-        for f in fnf {
-            print("\(f)")
-        }
-}
-
-if CommandLine.argc == 2 {
-    let apath = CommandLine.arguments[1]
-    let fnf = try FM.contentsOfDirectory(atPath: apath)
-    print("Listing files in directory \(apath).")
-    for f in fnf {
-        print("\(f)")
-    }
-}
-
-if CommandLine.arguments[1] == "-lS" {
-            let bpath = CommandLine.arguments[2]
-            let fnf = try FM.subpathsOfDirectory(atPath: bpath)
-            print("Listing subdirectories in directory \(bpath).")
-            for f in fnf {
-                print("\(f)")
-            }
-}
+let pname = "ls"
--- Sources/mv/main.swift
+++ Sources/mv/main.swift
@@ -11,18 +11,9 @@
 import Foundation
 
 var FM = FileManager.default
-var progName = CommandLine.arguments[0]
 
-if CommandLine.argc == 1 {
-    print("\(progName): missing operand")
-    exit(1)
-}
+var argc = CommandLine.argc
+var argv = CommandLine.arguments
 
-var fp1 = CommandLine.arguments[1]
-var fp2 = CommandLine.arguments[2]
+let pname = ""
 
-do {
-    try FM.moveItem(atPath: fp1, toPath: fp2)
-} catch {
-    print(error)
-}
