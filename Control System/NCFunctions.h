26f4dd62f42603eb8acc9dda8fa0bec410970fc1
MP Prabhanand <wdxp@rediffmail.com>
2022-03-29 19:50:55 +0530

    coreutilsctl update

--- Control System/NCFunctions.h
+++ Control System/NCFunctions.h
@@ -4,6 +4,7 @@
 #include <sys/types.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/utsname.h>
 #include "NCCoreFunctions.h"
 
 int printAbout() {
@@ -11,4 +12,14 @@ int printAbout() {
     printf("Copyright (c) 2022 WDXP.\n\n");
     printf("This program is free software, licensed under the MIT License.\n");
     printf("Want help? Try `coreutilsctl --help`!\n");
+}
+
+int getOperatingSystemName() {
+    struct utsname uts;
+
+    if(uname(&uts) < 0)
+        perror("Error: Cannot get OS.");
+    else {
+        printf("Detected OS as %s.\n", uts.sysname);
+    }
 }
