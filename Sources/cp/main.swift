940e5a60f57210467b8ee0324e7a25e931761edc
MP Prabhanand <wdxp@rediffmail.com>
2022-03-29 18:33:02 +0530

    Rework cp

--- Sources/cp/main.swift
+++ Sources/cp/main.swift
@@ -17,3 +17,24 @@ var argv = CommandLine.arguments
 
 let pname = "cp"
 
+func copyFile(from: String, to: String) {
+    do {
+        try FM.copyItem(atPath: from, toPath: to)
+    } catch {
+        print("\(pname): \(error)")
+    }
+}
+
+if argc == 1 {
+    print("\(pname): no file specified")
+    exit(1)
+}
+
+if argc == 2 {
+    print("\(pname): missing destination file operand after '\(argv[1])'")
+    exit(1)
+}
+
+if argc == 3 {
+    copyFile(from: argv[1], to: argv[2])
+}
